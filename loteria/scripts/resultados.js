dcca1a8ea7c1476f90a188ad7c0bf44680c99998
Franco Pinedo <francopinedo23@gmail.com>
2023-09-16 22:46:17 -0300

    Api agregada

--- loteria/scripts/resultados.js
+++ loteria/scripts/resultados.js
@@ -1,18 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const resultadosLista = document.getElementById('resultados-lista');
 
-    // Realiza una solicitud GET a la API de resultados de lotería
-    fetch('URL_DE_TU_API')
+function obtenerDatosDeLoteria() {
+    // URL de la API (reemplaza con la URL real de tu API)
+    const url = 'https://artesting.apuestasroyal.com/apiRoyal/resultados/2023-09-14';
+    const resultadosLista = document.getElementById('resultados-lista');
+    
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Recorre los resultados y crea elementos HTML para mostrarlos
-            data.resultados.forEach(resultado => {
+            // Itera a través de los datos y muestra cada elemento en la consola
+            data.forEach(item => {
+                console.log('Lotería:', item.loteria);
+                console.log('Número:', item.numero);
+                console.log('Nombre:', item.nombre);
+                console.log('Hora:', item.hora);
+                console.log('Fecha:', item.fecha);
+                console.log('-------------------');
+
+                // Crea elementos de lista y agrégalos al documento HTML
                 const li = document.createElement('li');
-                li.textContent = `Lotería: ${resultado.nombre}, Número Ganador: ${resultado.numero}`;
+                li.textContent = `Lotería: ${item.nombre}, Número Ganador: ${item.numero}`;
                 resultadosLista.appendChild(li);
             });
         })
         .catch(error => {
-            console.error('Error al obtener resultados de la API:', error);
+            console.error('Error al obtener datos de la API:', error);
         });
-});
+}
+
+// Llama a la función para obtener los datos de la lotería cuando se carga la página
+document.addEventListener('DOMContentLoaded', obtenerDatosDeLoteria);
