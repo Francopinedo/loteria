15a274a0eae5788864bf38fd026d85ed9e2f1fe3
Eduardo Marquez <108445068+EduardoMarqz@users.noreply.github.com>
2023-09-17 01:04:55 -0300

    Agregado de script de link dinámico

---
+++ scripts/resultados.js
@@ -0,0 +1,32 @@
+import { generarNuevoLink } from './link_api_resultado.js';
+
+function obtenerDatosDeLoteria() {
+    // URL de la API (reemplaza con la URL real de tu API)
+    const url = generarNuevoLink();
+    const resultadosLista = document.getElementById('resultados-lista');
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Itera a través de los datos y muestra cada elemento en la consola
+            data.forEach(item => {
+                console.log('Lotería:', item.loteria);
+                console.log('Número:', item.numero);
+                console.log('Nombre:', item.nombre);
+                console.log('Hora:', item.hora);
+                console.log('Fecha:', item.fecha);
+                console.log('-------------------');
+
+                // Crea elementos de lista y agrégalos al documento HTML
+                const li = document.createElement('li');
+                li.textContent = `Lotería: ${item.nombre}, Número Ganador: ${item.numero}, Hora: ${item.hora}`;
+                resultadosLista.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener datos de la API:', error);
+        });
+}
+
+// Llama a la función para obtener los datos de la lotería cuando se carga la página
+document.addEventListener('DOMContentLoaded', obtenerDatosDeLoteria);
